b78280973f966f40591f5391a0a928d85ad3cced
Naravi <170275956+NaraAvi@users.noreply.github.com>
2026-07-08 16:06:40 +0200

    Add grid cell fill effect on mouse hover over
    -Added event listenter to "container" div to listen for mouseover/hover
    -Initiated a  callback function on event dispatch to attach a css class to
    event target
    -Added a css class to change background color of .col element to red

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 (() => {
     let gridSize = 16;
 
+    const container = AddGrid(gridSize);
+    container.addEventListener('mouseover', (event) => {
+        event.target.classList.add('fill');
+    });
+
+})();
+
+
+function AddGrid(gridSize) {
     const container = document.querySelector(".container");
 
-    for(i = 0; i <= gridSize; i++){
+    for (i = 0; i <= gridSize; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
-        container.append(row);    
+        container.append(row);
 
-        for(n = 0; n <= gridSize; n++){
+        for (n = 0; n <= gridSize; n++) {
             const col = document.createElement("div");
             col.classList.add('col');
             row.appendChild(col);
         }
     }
 
+    return container;
+}
 
-})();
+
+
+
