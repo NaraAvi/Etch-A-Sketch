4fc5f9856a141ba3fe572a33ae0ff0641eff2ccc
Naravi <170275956+NaraAvi@users.noreply.github.com>
2026-07-08 16:54:05 +0200

    Add custom grid size with user input
    -Added button to prompt user for custom grid size
    -Added validation to ensure user grid size is:
    0 <= userInput <= 10-
    -Updated AddGrid method to remove all children of parent container element
    if it has any children

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
 (() => {
-    let gridSize = 16;
+    let gridSize = 16;  
+    
 
     const container = AddGrid(gridSize);
+
+    //Adds a css class on mouse hover to the target element/s of the container div through the Event Bubbling mechanism
     container.addEventListener('mouseover', (event) => {
         event.target.classList.add('fill');
     });
 
+    const btnGridSize = document.querySelector("#btn-grid-size");
+
+    btnGridSize.addEventListener("click", () => {
+        let userInput = parseInt(prompt("Enter a value between 0-100:"));
+        
+        //Validate userInput and alert the user if it is not valid
+        if (!(Number.isInteger(userInput)) || userInput > 100)   
+            alert("Error: This is not a valid number.");
+        else
+            AddGrid(userInput);
+    });
+
 })();
 
-
+//Creates a grid of variable size with gridSize num of rows and gridSize num of columns within each row and returns the container
 function AddGrid(gridSize) {
     const container = document.querySelector(".container");
+    console.log(gridSize);
+    if (container.childElementCount !== 0){
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+    }
+
+    gridSize = gridSize > 100 ? 100 : gridSize;
 
     for (i = 0; i <= gridSize; i++) {
         const row = document.createElement('div');
@@ -23,7 +46,6 @@ function AddGrid(gridSize) {
             row.appendChild(col);
         }
     }
-
     return container;
 }
 
